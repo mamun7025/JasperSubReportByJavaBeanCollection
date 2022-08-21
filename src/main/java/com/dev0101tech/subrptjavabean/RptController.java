0df16f48b116518dc6c4c60d4932221345614e3d
mamun7025 <mamun7025@gmail.com>
2022-08-22 00:52:40 +0400

    update report files and dynamic path

--- src/main/java/com/dev0101tech/subrptjavabean/RptController.java
+++ src/main/java/com/dev0101tech/subrptjavabean/RptController.java
@@ -7,13 +7,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Controller
 @RequestMapping("/api")
 public class RptController {
+
 
     public void renderRpt(){
         String masterReportFileName = "D://zProfessional_Projects/dev-workspace/jasper-reports/jspr-sub-rpt-javabean/jsprsubrptjavabean/src/main/resources/reports" + "/jasper_report_template.jrxml";
@@ -28,7 +35,7 @@ public class RptController {
             JasperReport jasperMasterReport = JasperCompileManager.compileReport(masterReportFileName);
             JasperReport jasperSubReport = JasperCompileManager.compileReport(subReportFileName);
 
-            Map<String, Object> parameters = new HashMap<String, Object>();
+            Map<String, Object> parameters = new HashMap<>();
             parameters.put("subreportParameter", jasperSubReport);
             JasperFillManager.fillReportToFile(jasperMasterReport, destFileName, parameters, beanColDataSource);
         } catch (JRException e) {
@@ -38,18 +45,30 @@ public class RptController {
     }
 
 
-    public byte[] renderRptByte(){
-        String masterReportFileName = "D://zProfessional_Projects/dev-workspace/jasper-reports/jspr-sub-rpt-javabean/jsprsubrptjavabean/src/main/resources/reports" + "/jasper_report_template.jrxml";
-        String subReportFileName = "D://zProfessional_Projects/dev-workspace/jasper-reports/jspr-sub-rpt-javabean/jsprsubrptjavabean/src/main/resources/reports" + "/address_report_template.jrxml";
+    public byte[] renderRptByte() throws URISyntaxException {
+        String  jasperRptFileName = "master_report";
+        String  jasperSubRptFileName = "subreport_report";
+        // Get absolute report folder path
+//        File file = ResourceUtils.getFile("classpath:reports");
+        URL res = getClass().getClassLoader().getResource("reports");
+        assert res != null;
+        File file = Paths.get(res.toURI()).toFile();
+        String baseRptFolderPath = file.getAbsolutePath();
+        System.out.println("Absolute path is : " + baseRptFolderPath);
+        String fileSeparator = FileSystems.getDefault().getSeparator();
+        String targetReportPath = baseRptFolderPath + fileSeparator + jasperRptFileName + ".jrxml";
+        String subReportPath  =  baseRptFolderPath + fileSeparator;
+        System.out.println("Target report path is : "+targetReportPath);
 
         DataBeanList DataBeanList = new DataBeanList();
         ArrayList<DataBean> dataList = DataBeanList.getDataBeanList();
         JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
         try {
             /* Compile the master and sub report */
-            JasperReport jasperMasterReport = JasperCompileManager.compileReport(masterReportFileName);
-            JasperReport jasperSubReport = JasperCompileManager.compileReport(subReportFileName);
-            Map<String, Object> parameters = new HashMap<String, Object>();
+            JasperCompileManager.compileReportToFile(subReportPath + jasperSubRptFileName +  ".jrxml"); // compile it through IDE
+            JasperReport jasperMasterReport = JasperCompileManager.compileReport(targetReportPath);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("SUBREPORT_DIR", subReportPath);
             // Fill the report
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperMasterReport, parameters, beanColDataSource);
             // Export the report to a PDF file
@@ -61,8 +80,8 @@ public class RptController {
     }
 
     @RequestMapping("/get-rpt")
-    public ResponseEntity<byte[]> getRpt(){
-//        renderRpt();
+    public ResponseEntity<byte[]> getRpt() throws URISyntaxException {
+        // renderRpt();
         String filename = "addressReport";
         byte[] data = renderRptByte();
         HttpHeaders headers = new HttpHeaders();
